49154eb342c97a6ede926b2dd76748bf708b96fd
Antxon <antxonegi@gmail.com>
2020-02-19 12:35:09 +0100

    Animaciones hechas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CapitalizePipe } from './capitalize.pipe';
+import { SemaforoComponent } from './semaforo/semaforo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CapitalizePipe
+    CapitalizePipe,
+    SemaforoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/semaforo/semaforo.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+@Component({
+  selector: 'app-semaforo',
+  templateUrl: './semaforo.component.html',
+  styleUrls: ['./semaforo.component.css'],
+  animations: [
+    trigger('cambiaColor', [
+      state('rojo', style({
+        backgroundColor: 'red'
+      })),
+      state('verde', style({
+        backgroundColor: 'green',
+        transform: 'scale(1.5)'
+      })),
+      state('amarillo', style({
+        backgroundColor: 'yellow',
+        transform: 'translateX(100px)'
+      })),
+      transition('rojo => amarillo', animate(500)),
+      transition('amarillo => verde', animate('1s ease-in')),
+      transition('verde => rojo', animate(200)),
+      transition('void => *', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0
+        }),
+        animate(1000)
+      ])
+    ])
+  ]
+})
+export class SemaforoComponent implements OnInit {
+
+  color: string;
+
+  constructor() {
+    this.color = 'rojo';
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      if (this.color === 'rojo') {
+        this.color = 'amarillo';
+      } else if (this.color === 'amarillo') {
+        this.color = 'verde';
+      } else if (this.color === 'verde') {
+        this.color = 'rojo';
+      }
+    }, 2000);
+  }
+
+}
